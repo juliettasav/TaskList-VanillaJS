e6a77d63dd7db52acd99edaf06308331c9fb7463
Yuliia Savdeieva <yuliia.savdeieva@Yuliias-MacBook-Pro.local>
2020-03-23 16:35:42 +0200

    delete and filter tasks

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ const taskInput = document.querySelector('#task');
 loadAllEventListeners();
 
 function loadAllEventListeners(){
+
     form.addEventListener('submit', addTask);
+
+    taskList.addEventListener('click', removeTask);
+
+    clearBtn.addEventListener('click', clearAllTasks);
+
+    filter.addEventListener('keyup', filterTasks)
 }
 
 function addTask(e) {
@@ -34,4 +41,38 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+function removeTask(e) {
+
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+        }
+        
+    }
+
+}
+
+function clearAllTasks() {
+
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+        
+    })
+
+    
 }
